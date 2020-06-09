485e057bf0e90e3adf8189f7e89a16bc68551ea1
Dementev Slava <kursant344@gmail.com>
2020-06-09 15:37:17 +0300

    add tests

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-[![Build status](https://ci.appveyor.com/api/projects/status/vcq0r8hepaep6rut?svg=true)](https://ci.appveyor.com/project/DementevSlava/aqa-2-3-1-deliverychangedate)
+[![Build status](https://ci.appveyor.com/api/projects/status/0sfuwh5ai0b445tj?svg=true)](https://ci.appveyor.com/project/DementevSlava/aqa-2-3-2-testmode)
--- src/test/java/ru/netology/AuthTest.java
+++ src/test/java/ru/netology/AuthTest.java
@@ -1,5 +1,6 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.RegistrationDto;
@@ -21,7 +22,40 @@ public class AuthTest {
         $("[name=login]").setValue(validValidActiveUser.getLogin());
         $("[name=password]").setValue(validValidActiveUser.getPassword());
         $(".button__text").click();
-        $(".App_appContainer__3jRx1 h2.heading").waitUntil(exist, 5000);
-        $(".App_appContainer__3jRx1 h2.heading").shouldHave(matchesText("Личный кабинет"));
+        $("[class='heading heading_size_l heading_theme_alfa-on-white']").waitUntil(exist, 5000);
+        $("[class='heading heading_size_l heading_theme_alfa-on-white']").shouldHave(matchesText("Личный кабинет"));
+    }
+
+    @Test
+    void shouldSubmitBlockedUser() {
+        open("http://localhost:9999");
+        RegistrationDto validValidAcBlockedUser = getValidBlockedUser();
+        $("[name=login]").setValue(validValidAcBlockedUser.getLogin());
+        $("[name=password]").setValue(validValidAcBlockedUser.getPassword());
+        $(".button__text").click();
+        $(".notification_status_error").waitUntil(visible, 5000);
+        $(".notification_visible[data-test-id=error-notification]").shouldHave(Condition.matchesText("Ошибка! Пользователь заблокирован"));
+    }
+
+    @Test
+    void shouldSubmitWithIncorrectPassword() {
+        open("http://localhost:9999");
+        RegistrationDto userWithIncorrectPassword = getUserWithIncorrectPassword();
+        $("[name=login]").setValue(userWithIncorrectPassword.getLogin());
+        $("[name=password]").setValue(userWithIncorrectPassword.getPassword());
+        $(".button__text").click();
+        $(".notification_status_error").waitUntil(visible, 5000);
+        $(".notification_visible[data-test-id=error-notification]").shouldHave(Condition.matchesText("Ошибка! Неверно указан логин или пароль"));
+    }
+
+    @Test
+    void shouldSubmitWithIncorrectLogin() {
+        open("http://localhost:9999");
+        RegistrationDto userWithIncorrectLogin = getUserWithIncorrectLogin();
+        $("[name=login]").setValue(userWithIncorrectLogin.getLogin());
+        $("[name=password]").setValue(userWithIncorrectLogin.getPassword());
+        $(".button__text").click();
+        $(".notification_status_error").waitUntil(visible, 5000);
+        $(".notification_visible[data-test-id=error-notification]").shouldHave(Condition.matchesText("Ошибка! Неверно указан логин или пароль"));
     }
 }
